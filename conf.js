bdfcf36ed8543c7d90871dfcea7e1b4be0f9c5b2
HamzahMheidat <hamzahmheidat96@github.com>
2019-09-04 10:46:29 +0300

    fixs

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 var { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-    directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
@@ -14,7 +14,7 @@ exports.config = {
     specs: ['article-seo-sharing-tags/seo-sharing-tags.specs.js'],
     onPrepare: () => {
         global.EC = protractor.ExpectedConditions;
-        global.WAIT_DURATION = 5000;
+        global.WAIT_DURATION = 20000;
         global.ARTICLE_URL = 'https://www.cosmopolitan.com/style-beauty/fashion/g28749279/types-of-coats/';
         browser.waitForAngularEnabled(false);
         jasmine.getEnv().addReporter(new SpecReporter({
